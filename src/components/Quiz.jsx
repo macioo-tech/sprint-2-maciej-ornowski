b70c5189989a909b19553f372828f19d210fd5f7
mac <orno.morsoft@gmail.com>
2025-02-03 01:12:05 +0100

    02-02-25 first version of quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,10 +10,13 @@ const Quiz = () => {
     correctAnswers: 0,
     wrongAnswers: 0,
   });
+  const [summary, setSummary] = useState(false);
 
   const { text, answers } = QUESTIONS[activeQuestion];
   console.log("Question", text);
   answers.forEach((item) => console.log(item));
+  console.log("Długość quizu:", QUESTIONS.length);
+  console.log("Aktualne pytanie:", activeQuestion);
 
   const handleClick = (correct) => {
     if (correct) {
@@ -28,20 +31,37 @@ const Quiz = () => {
         wrongAnswers: prev.wrongAnswers + 1,
       }));
     }
-    setActiveQuestion(activeQuestion + 1);
+
+    if (activeQuestion < QUESTIONS.length - 1) {
+      setActiveQuestion(activeQuestion + 1);
+    } else {
+      setActiveQuestion(0);
+      setSummary(true);
+    }
   };
 
   return (
     <div>
-      <h1>Quiz</h1>
-      <h2>{text}</h2>
-      {answers.map((el, i) => (
-        <Button
-          value={el.text}
-          onClick={() => handleClick(el.isCorrect)}
-          key={i}
-        ></Button>
-      ))}
+      {!summary ? (
+        <div>
+          <h1>Quiz</h1>
+          <h2>{text}</h2>
+          {answers.map((el, i) => (
+            <Button
+              value={el.text}
+              onClick={() => handleClick(el.isCorrect)}
+              key={i}
+            ></Button>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h1>Podsumowanie:</h1>
+          <p>Poprawnych: {result.correctAnswers}</p>
+          <p>Błędnych: {result.wrongAnswers}</p>
+          <Button value="Rozpocznij od nowa"></Button>
+        </div>
+      )}
     </div>
   );
 };
